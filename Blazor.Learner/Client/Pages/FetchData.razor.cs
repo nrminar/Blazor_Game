8204bd21cdafa247782e6dc957be1581369575d3
Brad Hennes <brhennes@gmail.com>
2020-10-09 13:50:37 -0500

    code-behind example

---
+++ Blazor.Learner/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.Learner.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Blazor.Learner.Client.Pages
+{
+    public class FetchForecastData : ComponentBase
+    {
+        public WeatherForecast[] forecasts;
+        
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("/api/weatherforecast");
+        }
+    }
+}
